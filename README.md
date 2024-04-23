39d7cb2f6f462e7857743f6aff0c2234bd69b1e6
matwiejuk20p <91263582+matwiejuk20p@users.noreply.github.com>
2024-04-23 10:11:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# karaoke_lyrics-finder
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/matwiejuk20p/karaoke_lyrics-finder)
---
+++ lyrics-finder/src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+import Axios from 'axios';
+import { useState } from 'react';
+
+function App() {
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+  const [lyrics, setLyrics] = useState('');
+
+  function searchLyrics() {
+    if (artist === '' || song === '') {
+      return;
+    }
+    Axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`).then((res) => {
+      console.log(res.data.lyrics);
+      setLyrics(res.data.lyrics);
+    });
+  }
+
+  return (
+    <div className="App">
+      <h1>KARAOKE</h1>
+
+      <input
+        className="inp"
+        type="text"
+        placeholder="Artist name"
+        onChange={(e) => {
+          setArtist(e.target.value);
+        }}
+      />
+      <input
+        className="inp"
+        type="text"
+        placeholder="Song name"
+        onChange={(e) => {
+          setSong(e.target.value);
+        }}
+      />
+      <button className="btn" onClick={() => searchLyrics()}>
+        WYSZUKAJ
+      </button>
+      <hr />
+      <pre>{lyrics}</pre>
+    </div>
+  );
+}
+
+export default App;
